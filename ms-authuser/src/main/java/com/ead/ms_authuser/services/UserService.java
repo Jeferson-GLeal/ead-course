5bd8df63412e0f5ec62676e7bba88489be2971b4
Jeferson-Leal <jeferson.gleal@outlook.com>
2024-10-23 19:52:52 -0300

    [feat] adicionado a classe AuthenticationController.

--- ms-authuser/src/main/java/com/ead/ms_authuser/services/UserService.java
+++ ms-authuser/src/main/java/com/ead/ms_authuser/services/UserService.java
@@ -7,7 +7,16 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
+
     List<UserModel> findAll();
+
     Optional<UserModel> findById(UUID userId);
+
     void delete(UserModel userModel);
+
+    void save(UserModel userModel);
+
+    boolean existsByUserName(String username);
+
+    boolean existsByEmail(String email);
 }
